d93b1331a5382bd8e957fdbd7c2c252daf21977d
Jenish Mor <jenishmor@gmail.com>
2025-12-27 14:16:06 +0530

    test: modify workflow to add blob reports after sharding and disabled vercel preview on every push

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,12 +21,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html'],
-    ['json', { outputFile: 'test-results/results.json' }],
-    ['junit', { outputFile: 'test-results/junit.xml' }],
-    ['list']
-  ],
+  reporter: process.env.CI 
+    ? [['blob'], ['list']] // CI: Use blob for merging, list for logs
+    : [
+        ['html'],
+        ['json', { outputFile: 'test-results/results.json' }],
+        ['junit', { outputFile: 'test-results/junit.xml' }],
+        ['list']
+      ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
